fa16f85d645dbbd6b2d1b3ded335c5d6bf820400
Hoxtygen <hoxtygen@live.com>
2019-10-01 14:10:19 +0100

    Finish scopes

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,12 +5,13 @@
 x = 12
 
 def changeX():
+    global x
     x = 99
-
+   
 changeX()
+print(x)
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
-print(x)
 
 
 # This nested function has a similar problem.
@@ -19,6 +20,7 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
